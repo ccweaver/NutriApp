94bf989e372902e90a39c95a391fe23f6f635ce1
ccweaver <ccweaver@princeton.edu>
2014-09-13 12:43:52 -0400

    lots of changes... i'll be more specific
    added Addition model, saving things to db when creating dishes

---
+++ nutri/added_ingreds/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from ingred_table.models import Ingredient
+
+class Addition(models.Model):
+	ingred = models.ForeignKey(Ingredient)
+	amount_grams = models.DecimalField(max_digits=10, decimal_places=2)
+
+	def __unicode__(self):
+		return u'%s IN: %s' % (self.ingred, self.dish)
