dc2944d3f871ea0030c56e0b41c4f95cc6bf6d6c
charlie1515 <charles.stanton1515@gmail.com>
2015-03-05 17:47:25 -0500

    Update models.py

--- nutri/Restaurant/models.py
+++ nutri/Restaurant/models.py
@@ -10,7 +10,7 @@ class Restaurant(models.Model):
 	zipcode = models.IntegerField()
 	user = models.ForeignKey(User)
 	website = models.CharField(max_length=300)
-	phone = models.IntegerField()
+	
 	
 
 	def __unicode__(self):
