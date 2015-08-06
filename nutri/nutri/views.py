1037ec3843fcc87cc9fdc8d5596d46544c7c06ac
charlie1515 <charles.stanton1515@gmail.com>
2015-08-06 14:34:58 -0400

    Update views.py

--- nutri/nutri/views.py
+++ nutri/nutri/views.py
@@ -613,7 +613,8 @@ def restaurant_profile(request, rid):
         strings.append("%d" % gfat)
         strings.append("%d" % gcarb)
         strings.append("%d" % gsug)
-        strings.append("%d" % mgna , mgno)
+        strings.append("%d" % mgna)
+        strings.append("%d" % mgno)
         strings.append(price)
         strings.append(description)
       
