2427a91a8f258d108f63f98eb1ffe3409f8c9859
ccweaver <ccweaver@princeton.edu>
2015-08-05 22:39:23 -0400

    fixing next page button

--- nutri/nutri/views.py
+++ nutri/nutri/views.py
@@ -161,7 +161,7 @@ def search_results(request, term, page=1):
         low_index = 20*(int(page)-1)
         high_index = 20* (int(page))
         rs_20 = rs[low_index:high_index]
-    return render(request, 'search_results.html', {'rests':rs_20, 'num_rests':len(rs), 'page':page, 'term':term})
+    return render(request, 'search_results.html', {'rests':rs_20, 'num_rests':len(rs), 'page':int(page), 'page_mult20':int(page)*20, 'term':term})
 
 
 
