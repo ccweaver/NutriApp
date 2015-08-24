0fd4bd443f24cb58794a1847f7530e22c0404d62
charlie1515 <charles.stanton1515@gmail.com>
2015-08-23 23:44:45 -0400

    Update views.py

--- nutri/nutri/views.py
+++ nutri/nutri/views.py
@@ -118,6 +118,7 @@ def search_results(request, term, page=1):
     # Zip Code Search
     #################
     zRE = re.compile("^[0-9][0-9][0-9][0-9][0-9]$")
+    a = ''
     if zRE.match(term):
         restaurants = Restaurant.objects.all().order_by('zipcode').order_by('street', 'number')
         rs = []
@@ -138,8 +139,8 @@ def search_results(request, term, page=1):
 
     #################
     # City Search
-    #################
-    elif match(term):
+    ################
+    elif a.match(term):
         r_citySorted = Restaurant.objects.filter(city__icontains=term).order_by('street', 'number')
         rs = []
         for r in r_citySorted:
