2a9b28d7cb6d472524116ccc0a589d1352cf85c1
charlie1515 <charles.stanton1515@gmail.com>
2015-08-23 23:39:12 -0400

    Update views.py

--- nutri/nutri/views.py
+++ nutri/nutri/views.py
@@ -139,7 +139,8 @@ def search_results(request, term, page=1):
     #################
     # City Search
     #################
-    elif r_citySorted = Restaurant.objects.filter(city__icontains=term).order_by('street', 'number')
+    elif match(term):
+        r_citySorted = Restaurant.objects.filter(city__icontains=term).order_by('street', 'number')
         rs = []
         for r in r_citySorted:
             bool_dm = False
