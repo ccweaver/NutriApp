c272e712394a8f5b20e88496cdf686966769347f
charlie1515 <charles.stanton1515@gmail.com>
2015-10-28 14:01:32 -0400

    Update views.py

--- nutri/nutri/views.py
+++ nutri/nutri/views.py
@@ -569,7 +569,7 @@ def restaurant_profile(request, rid):
         jazz_man = True
     print 'This is jazz_man', jazz_man
   
-    if "Tresser Blvd" in restaurant.street:
+    if "California Pizza Kitchen" in restaurant.name:
         town_center = True
     print 'This is town_center', town_center
     
