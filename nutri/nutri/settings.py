fdaf16db4ef479d3e10197563cf1651b3059cc08
ccweaver <ccweaver@princeton.edu>
2016-03-12 13:33:13 -0500

    static_root added

--- nutri/nutri/settings.py
+++ nutri/nutri/settings.py
@@ -97,4 +97,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_ROOT= os.path.join('nutri','static')
 STATIC_URL = '/static/'
+
